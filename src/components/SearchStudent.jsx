a89c79032734b8a5ac320a1922dc49d8e8d6cc86
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-08 23:04:51 +0530

    data collected

--- src/components/SearchStudent.jsx
+++ src/components/SearchStudent.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const SearchStudent = () => {
+
+const [data,setData]=useState(
+
+    {
+        "name":""
+    }
+
+)
+const inputHandler = (event) => {
+
+        setData({...data,[event.target.name]:event.target.value})
+
+}
+
+const readvalue = () => {
+
+console.log(data)
+
+}
+
+
+
+
+
   return (
     <div>
         <Navbar/>
@@ -15,12 +39,12 @@ const SearchStudent = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
             <label htmlFor="" className="fomr-label">Name</label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
                     
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                <button className="btn btn-info"><b>Search</b></button>
+                <button className="btn btn-info" onClick={readvalue}><b>Search</b></button>
 
                 </div>
             </div>
