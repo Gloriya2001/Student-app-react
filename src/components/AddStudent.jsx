2e2488b258a31e1143fd3826b1f491265e62e613
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 00:11:14 +0530

    add updated

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 const AddStudent = () => {
 
@@ -8,16 +9,16 @@ const AddStudent = () => {
     {
       "name": "",
       "rollNo": "",
-      "admNo":"",
-      "parentName":"",
-      "collegeName":"",
-      "dob":"",
-      "emailId":"",
-      "password":"",
-      "confirmPassword":"",
-      "img":"",
-      
-    
+      "admNo": "",
+      "parentName": "",
+      "collegeName": "",
+      "dob": "",
+      "emailId": "",
+      "password": "",
+      "confirmPassword": "",
+      "img": "",
+
+
     }
 
   )
@@ -28,9 +29,17 @@ const AddStudent = () => {
 
   }
 
-  const readValue=() => {
+  const readValue = () => {
 
     console.log(data)
+    axios.post("",data).then(
+      console.log(response.data)
+      if (response.data.status == "success") {
+        alert("Successfully detail Added")
+      } else {
+        alert("Error!!!!")
+      }
+    )
   }
 
 
@@ -50,7 +59,7 @@ const AddStudent = () => {
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <label htmlFor="" className="form-label" >Name</label>
-                <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
+                <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
@@ -68,43 +77,43 @@ const AddStudent = () => {
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <label htmlFor="" className="form-label">Parent Name</label>
-                <input type="text" className="form-control" name='parentName' value={data.parentName} onChange={inputHandler}/>
+                <input type="text" className="form-control" name='parentName' value={data.parentName} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                 <label htmlFor="" className="form-label">College Name</label>
-                <input type="text" className="form-control" name='collegeName' value={data.collegeName} onChange={inputHandler}/>
+                <input type="text" className="form-control" name='collegeName' value={data.collegeName} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <label htmlFor="" className="form-label">date of birth</label>
-                <input type="date"  id="" className="form-control" name='dob' value={data.dob} onChange={inputHandler}/>
+                <input type="date" id="" className="form-control" name='dob' value={data.dob} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <label htmlFor="" className="form-label">Email-id</label>
-                <input type="text" className="form-control" name='emailId' value={data.emailId} onChange={inputHandler}/>
+                <input type="text" className="form-control" name='emailId' value={data.emailId} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">Password</label>
-                <input type="password"  id="" className="form-control" name='password' value={data.password} onChange={inputHandler}/>
+                <input type="password" id="" className="form-control" name='password' value={data.password} onChange={inputHandler} />
 
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <lablel className="form-label">Confirm Password</lablel>
-                <input type="password"  id="" className="form-control" name='confirmPassword' value={data.confirmPassword}  onChange={inputHandler}/>
+                <input type="password" id="" className="form-control" name='confirmPassword' value={data.confirmPassword} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                 <lablel className="form-label">upload Photo</lablel>
-                <input type="file"  id="" className="form-control" name='img' value={data.img}  onChange={inputHandler}/>
+                <input type="file" id="" className="form-control" name='img' value={data.img} onChange={inputHandler} />
 
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
